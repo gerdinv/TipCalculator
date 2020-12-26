3a3006e6a1ff5be330be59871bbebd6007bda9fe
Gerdin <gerdinventura2@gmail.com>
2020-12-26 05:47:21 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Gerdin Ventura on 12/26/20.
+//  Copyright © 2020 Gerdin Ventura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
