b9985d6affca92311b6e7bcf2aa41c0705776ac3
Gerdin <gerdinventura2@gmail.com>
2020-12-27 00:31:57 -0500

    Added UI Slider, Split feature, and fixed logical bugs

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,42 +10,94 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var test: UILabel!
+    var percentage = 0.00; // Default Tip: 15%
+    var bill = 0.00;
+    var splitNum = 1.00;
+    
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipPercentage: UILabel!
     @IBOutlet weak var billField: UITextField!
-   
     @IBOutlet weak var tipLabel: UILabel!
-    
-    @IBOutlet weak var baba: UILabel!
-    
     @IBOutlet weak var tipControl: UISegmentedControl!
-
     @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var splitTotalLabel: UILabel!
+    @IBOutlet weak var splitControl: UISlider!
+    @IBOutlet weak var splitLabel: UILabel!
+    
+    @IBOutlet weak var topContentContainer: UIView!
+    @IBOutlet weak var middleContentContainer: UIView!
+    @IBOutlet weak var bottomContentContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topContentContainer.layer.cornerRadius = 15
+        topContentContainer.layer.masksToBounds = true
+        
+        middleContentContainer.layer.cornerRadius = 15
+        middleContentContainer.layer.masksToBounds = true
+        
+        bottomContentContainer.layer.cornerRadius = 15
+        bottomContentContainer.layer.masksToBounds = true
+        
+        tipSlider.maximumValue = 100;
     }
-
-
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
-    
-    @IBAction func calculateTip(_ sender: Any) {
-            //Get bill amount
-            let bill = Double(billField.text!) ?? 0
-
-            //Calculate the tip and total
-            let tipPercentages = [0.15, 0.18, 0.2]
-            let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-            let total = bill + tip
-
-            //Update the tip and total labels
-            tipLabel.text = String(format: "$%.2f", tip)
-            totalLabel.text = String(format: "$%.2f", total)
+    //calculates the tip % from the slider
+    @IBAction func updateTip(_ sender: UISlider) {
+        percentage = Double(Int(sender.value))
+        bill = calcTotal(percentage: percentage)
+        
+        updateLabels(bill: bill, splitNum: splitNum, percentage: percentage)
+        
+        tipControl.selectedSegmentIndex = UISegmentedControl.noSegment
     }
-   
-   
+    
+    //Calculates tip % when selecting from segment
+    @IBAction func updateTipFromSegment(_ sender: Any) {
+        let tipPercentages = [15, 18, 20]
+        
+        percentage = Double(tipPercentages[tipControl.selectedSegmentIndex])
+        bill = calcTotal(percentage: percentage)
+        
+        updateLabels(bill: bill, splitNum: splitNum, percentage: percentage)
+        
+        //Animates the slider to match the percentage selected from segment
+        UIView.animate(withDuration: 0.8, delay: 0.0, options: [], animations: {
+            self.tipSlider.setValue(Float(self.percentage), animated: true) },
+        completion: nil)
+    }
+    
+    //Calculates total tip when writing in text field
+    @IBAction func calculateTip(_ sender: Any) {
+        bill = calcTotal(percentage: percentage)
+        updateLabels(bill: bill, splitNum: splitNum, percentage: percentage)
+    }
+    
+    //Updates the split number from slider
+    @IBAction func updateSplit(_ sender: UISlider) {
+        splitNum = Double(Int(sender.value))
+        splitLabel.text = String(Int(splitNum))
+        splitTotalLabel.text = String(format: "$%.2f", (bill / splitNum))
+    }
+    
+    //Updates labels
+    func updateLabels(bill: Double, splitNum: Double, percentage: Double) {
+        tipPercentage.text = String("\(percentage)%")
+        splitTotalLabel.text = String(format: "$%.2f", (bill / splitNum))
+        totalLabel.text = String(format: "$%.2f", bill)
+    }
+    
+    //Caclulates the total Bill
+    func calcTotal( percentage: Double) -> Double{
+        bill = Double(billField.text!) ?? 0
+        return bill + (bill * Double(percentage / 100))
+    }
 }
 
